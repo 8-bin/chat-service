f4023801a301cacf0bfbc9d318a34c74a7578075
8-bin <taeho.kim0218@gmail.com>
2025-04-28 14:36:38 +0900

    Fix : Edit application.properties

--- src/main/java/com/example/chatservice/config/KafkaConfig.java
+++ src/main/java/com/example/chatservice/config/KafkaConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -20,16 +21,28 @@ public class KafkaConfig {
 
     public static final String CHAT_TOPIC = "chat";
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String securityProtocol;
+
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    private String saslMechanism;
+
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String saslJaasConfig;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("KAFKA_BOOTSTRAP_SERVERS"));
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-        config.put("security.protocol", System.getenv("SPRING_KAFKA_PROPERTIES_SECURITY_PROTOCOL"));
-        config.put("sasl.mechanism", System.getenv("SPRING_KAFKA_PROPERTIES_SASL_MECHANISM"));
-        config.put("sasl.jaas.config", System.getenv("SPRING_KAFKA_PROPERTIES_SASL_JAAS_CONFIG"));
+        config.put("security.protocol", securityProtocol);
+        config.put("sasl.mechanism", saslMechanism);
+        config.put("sasl.jaas.config", saslJaasConfig);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
@@ -42,15 +55,15 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("KAFKA_BOOTSTRAP_SERVERS"));
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "chat-group");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-        config.put("security.protocol", System.getenv("SPRING_KAFKA_PROPERTIES_SECURITY_PROTOCOL"));
-        config.put("sasl.mechanism", System.getenv("SPRING_KAFKA_PROPERTIES_SASL_MECHANISM"));
-        config.put("sasl.jaas.config", System.getenv("SPRING_KAFKA_PROPERTIES_SASL_JAAS_CONFIG"));
+        config.put("security.protocol", securityProtocol);
+        config.put("sasl.mechanism", saslMechanism);
+        config.put("sasl.jaas.config", saslJaasConfig);
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
